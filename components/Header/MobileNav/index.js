5bec43330c12719f596a647cfe49cd020808b316
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2022-07-07 18:33:59 +0100

    added framer motion to mobile menu

--- components/Header/MobileNav/index.js
+++ components/Header/MobileNav/index.js
@@ -4,12 +4,18 @@ import { components } from "../../../slices"
 import styles from "./styles.module.scss"
 import { MenuAlt3Icon } from "@heroicons/react/outline"
 import { PrismicNextImage } from "@prismicio/next"
+import { motion } from "framer-motion"
 
 export const MobileNav = ({ slices, image, primaryButton }) => {
   const [open, setOpen] = useState(false)
 
   const handleMenuState = () => {
     setOpen(!open)
+  }
+
+  const animateMenu = {
+    open: { opacity: 1 },
+    closed: { opacity: 0 },
   }
 
   return (
@@ -28,7 +34,12 @@ export const MobileNav = ({ slices, image, primaryButton }) => {
         <MenuAlt3Icon />
       </button>
 
-      <nav className={`${styles.nav} ${open && styles.open}`}>
+      <motion.nav
+        className={styles.nav}
+        animate={open ? "open" : "closed"}
+        variants={animateMenu}
+        transition={{ duration: 0.1 }}
+      >
         <ul className={styles.navList}>
           <SliceZone slices={slices} components={components} />
           <li>
@@ -37,7 +48,7 @@ export const MobileNav = ({ slices, image, primaryButton }) => {
             </PrismicLink>
           </li>
         </ul>
-      </nav>
+      </motion.nav>
     </div>
   )
 }
