8001954d907afbcd1d3bcd7ff23d9ce18957703c
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2022-07-13 16:13:25 +0100

    Removed animation from hero

--- slices/Hero/index.js
+++ slices/Hero/index.js
@@ -1,7 +1,6 @@
 import { PrismicNextImage } from "@prismicio/next"
 import { PrismicLink, PrismicRichText } from "@prismicio/react"
 import styles from "./styles.module.scss"
-import { useInView } from "react-intersection-observer"
 
 const Hero = ({
   slice: {
@@ -18,45 +17,39 @@ const Hero = ({
       image,
     },
   },
-}) => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-  })
-
-  return (
-    <section className={`container ${styles.section}`} ref={ref}>
-      <div>
-        {availability ? (
-          <p className={`${styles.availability} ${styles.available}`}>
-            Available For Hire
-          </p>
-        ) : (
-          <p className={`${styles.availability} ${styles.busy}`}>
-            Currently Booked Up!
-          </p>
-        )}
-        <div className={styles.eyebrow}>
-          <PrismicRichText field={eyebrow} />
-        </div>
-        <div className={styles.title}>
-          <PrismicRichText field={title} />
-        </div>
-        <div className={styles.buttonsFlex}>
-          <PrismicLink className="button primary" field={primaryButtonLink}>
-            {primaryButtonText}
-            <PrismicNextImage field={primaryButtonIcon} />
-          </PrismicLink>
-          <PrismicLink className="button secondary" field={secondaryButtonLink}>
-            {secondaryButtonText}
-            <PrismicNextImage field={secondaryButtonIcon} />
-          </PrismicLink>
-        </div>
+}) => (
+  <section className={`container ${styles.section}`}>
+    <div>
+      {availability ? (
+        <p className={`${styles.availability} ${styles.available}`}>
+          Available For Hire
+        </p>
+      ) : (
+        <p className={`${styles.availability} ${styles.busy}`}>
+          Currently Booked Up!
+        </p>
+      )}
+      <div className={styles.eyebrow}>
+        <PrismicRichText field={eyebrow} />
       </div>
-      <div className={`${styles.image} ${inView && styles.animate}`}>
-        <PrismicNextImage field={image} layout="responsive" priority={true} />
+      <div className={styles.title}>
+        <PrismicRichText field={title} />
       </div>
-    </section>
-  )
-}
+      <div className={styles.buttonsFlex}>
+        <PrismicLink className="button primary" field={primaryButtonLink}>
+          {primaryButtonText}
+          <PrismicNextImage field={primaryButtonIcon} />
+        </PrismicLink>
+        <PrismicLink className="button secondary" field={secondaryButtonLink}>
+          {secondaryButtonText}
+          <PrismicNextImage field={secondaryButtonIcon} />
+        </PrismicLink>
+      </div>
+    </div>
+    <div className={styles.image}>
+      <PrismicNextImage field={image} layout="responsive" priority={true} />
+    </div>
+  </section>
+)
 
 export default Hero
