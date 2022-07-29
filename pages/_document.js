691b83f590d1a67c51e4a98c141607d642fc66a5
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2022-07-29 09:44:59 +0100

    Switched back to Inter font family.

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400;500;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
         <script
